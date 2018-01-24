f8c6e468bfbc73c236d730a6a148ebe9089f6479
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-24 20:57:14 -0200

    Added user model

---
+++ lib/live_board_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule LiveBoardWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use LiveBoardWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(LiveBoardWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(LiveBoardWeb.ErrorView, :"404")
+  end
+end
---
+++ lib/live_board_web/views/user_view.ex
@@ -0,0 +1,17 @@
+defmodule LiveBoardWeb.UserView do
+  use LiveBoardWeb, :view
+  alias LiveBoardWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id,
+      name: user.name}
+  end
+end

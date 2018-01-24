a588cbfaf512344bc637ac40ca32b970f3e932d5
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-24 21:12:01 -0200

    Added routes

--- lib/live_board_web/router.ex
+++ lib/live_board_web/router.ex
@@ -8,4 +8,7 @@ defmodule LiveBoardWeb.Router do
   scope "/api", LiveBoardWeb do
     pipe_through :api
   end
+
+  resources "/users", UserController, except: [:show, :edit]
+  resources "/messages", MessageController, except: [:show, :edit]
 end

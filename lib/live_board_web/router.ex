d0ed73e4848131cce99775dbf7b26a434a575295
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-24 22:39:53 -0200

    Added home controller

--- lib/live_board_web/router.ex
+++ lib/live_board_web/router.ex
@@ -5,10 +5,24 @@ defmodule LiveBoardWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   scope "/api", LiveBoardWeb do
     pipe_through :api
 
     resources "/users", UserController, except: [:show, :edit]
     resources "/messages", MessageController, except: [:show, :edit]
+  end
+
+  scope "/", LiveBoardWeb do
+    pipe_through :browser
+
+    get "/*path", HomeController, :index
   end
 end
